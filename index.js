e97d5e59b4f3098a592a6bf0e38380af3d56cfbe
JNi - Jason Nickel <jni@vnr.de>
2018-09-11 10:26:21 +0200

    Init

---
+++ index.js
@@ -0,0 +1,47 @@
+module.exports = {
+  extends: 'airbnb-base',
+  env: {
+    browser: true
+  },
+  globals: {
+    __: false,
+    Ext: false,
+    i18n: false,
+    md5: false,
+    Phoenix: false,
+    APP_BASE_URL: false,
+    APP_URL: false,
+    phoenix: false,
+    phAppHelper: false,
+    APP_CONFIG: false,
+    APP_DEFAULT: false,
+    WEB_HELP_DEFAULT_URL: false,
+    LOCALE_CURRENT: false,
+    LOCALE_DATA: false,
+    __APP_VERSION__: false,
+    __ENABLE_RECORD_POINTER__: false,
+    __DEV__: false,
+    crosstab: false,
+    Clipboard: false,
+    karma: false,
+    _: false,
+    IBAN: false
+  },
+  rules: {
+    'comma-dangle': ['error', 'never'],
+    'func-names': ['error', 'never'],
+    'no-console': 'warn',
+    eqeqeq: 'warn',
+    'no-var': 'off',
+    'object-shorthand': 'off',
+    'prefer-rest-params': 'off',
+    'prefer-arrow-callback': 'off',
+    'prefer-template': 'off',
+    'one-var': 'off',
+    'prefer-destructuring': 'off',
+    'space-before-function-paren': 'off',
+    'quote-props': 'error',
+    'max-len': [2, 120, 4],
+    'prefer-spread': 'off'
+  }
+};
