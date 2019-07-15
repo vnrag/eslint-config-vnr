200b92fc96b29ecc9bbdb5003bdece95b9f902f6
Jason Nickel <jni@vnr.de>
2019-07-15 21:03:14 +0200

    Changed indentation rule; Added editorconfig; Updated dependencies

--- index.js
+++ index.js
@@ -1,66 +1,67 @@
 module.exports = {
-  extends: [
-    'airbnb'
-  ],
-  env: {
-    browser: true
-  },
-  globals: {
-    __: false,
-    Ext: false,
-    i18n: false,
-    md5: false,
-    Phoenix: false,
-    APP_BASE_URL: false,
-    APP_URL: false,
-    phoenix: false,
-    phAppHelper: false,
-    APP_CONFIG: false,
-    APP_DEFAULT: false,
-    WEB_HELP_DEFAULT_URL: false,
-    LOCALE_CURRENT: false,
-    LOCALE_DATA: false,
-    __APP_VERSION__: false,
-    __ENABLE_RECORD_POINTER__: false,
-    __DEV__: false,
-    crosstab: false,
-    Clipboard: false,
-    karma: false,
-    _: false,
-    IBAN: false
-  },
-  rules: {
-    'comma-dangle': ['error', 'never'],
-    'func-names': ['error', 'never'],
-    'no-console': 'warn',
-    eqeqeq: 'warn',
-    'no-var': 'off',
-    'object-shorthand': 'off',
-    'prefer-rest-params': 'off',
-    'prefer-arrow-callback': 'off',
-    'prefer-template': 'off',
-    'one-var': 'off',
-    'prefer-destructuring': 'off',
-    'space-before-function-paren': 'off',
-    'quote-props': 'error',
-    'max-len': [2, 120, 4],
-    'prefer-spread': 'off',
-    'import/no-unresolved': 'off',
-    'no-underscore-dangle': 'off',
-    'no-use-before-define': [
-      'error',
-      {
-        functions: false,
-        classes: false
-      }
+    extends: [
+        'airbnb'
     ],
-    'no-unused-vars': [
-      'warn',
-      {
-        argsIgnorePattern: '^(event|e|props)$'
-      }
-    ],
-    'react/no-unused-prop-types': 'warn',
-    'react/no-unused-state': 'warn'
-  }
+    env: {
+        browser: true
+    },
+    globals: {
+        __: false,
+        Ext: false,
+        i18n: false,
+        md5: false,
+        Phoenix: false,
+        APP_BASE_URL: false,
+        APP_URL: false,
+        phoenix: false,
+        phAppHelper: false,
+        APP_CONFIG: false,
+        APP_DEFAULT: false,
+        WEB_HELP_DEFAULT_URL: false,
+        LOCALE_CURRENT: false,
+        LOCALE_DATA: false,
+        __APP_VERSION__: false,
+        __ENABLE_RECORD_POINTER__: false,
+        __DEV__: false,
+        crosstab: false,
+        Clipboard: false,
+        karma: false,
+        _: false,
+        IBAN: false
+    },
+    rules: {
+        'comma-dangle': ['error', 'never'],
+        'func-names': ['error', 'never'],
+        'no-console': 'warn',
+        eqeqeq: 'warn',
+        'no-var': 'off',
+        'object-shorthand': 'off',
+        'prefer-rest-params': 'off',
+        'prefer-arrow-callback': 'off',
+        'prefer-template': 'off',
+        'one-var': 'off',
+        'prefer-destructuring': 'off',
+        'space-before-function-paren': 'off',
+        'quote-props': 'error',
+        indent: ['warn', 4],
+        'max-len': 'off',
+        'prefer-spread': 'off',
+        'import/no-unresolved': 'off',
+        'no-underscore-dangle': 'off',
+        'no-use-before-define': [
+            'error',
+            {
+                functions: false,
+                classes: false
+            }
+        ],
+        'no-unused-vars': [
+            'warn',
+            {
+                argsIgnorePattern: '^(event|e|props)$'
+            }
+        ],
+        'react/no-unused-prop-types': 'warn',
+        'react/no-unused-state': 'warn'
+    }
 };
